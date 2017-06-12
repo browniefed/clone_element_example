a52b77fc0ca90d004288e5e68a02cf0ba3996791
Jason Brown <browniefed@gmail.com>
2017-06-12 11:02:59 -0700

    Add code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-const AddPuncutation = ({ style, text, punctuation }) => <span style={style}>{text}{punctuation}</span>
+const AddPunctuation = ({ text, punctuation, ...props}) => <span {...props}>{text}{punctuation}</span>
 
 const Emphasize = ({ times, children }) => {
   return React.cloneElement(children, {
-    style: { fontWeight: "bold" },
-    punctuation: children.props.punctuation.repeat(times),
+    style: {
+      fontWeight: "bold"
+    },
+    punctuation: children.props.punctuation.repeat(times)
   })
 }
 
@@ -14,7 +16,7 @@ class App extends Component {
     return (
       <div>
         <Emphasize times={10}>
-          <AddPuncutation
+          <AddPunctuation
             text="Hello World"
             punctuation="!"
           />
